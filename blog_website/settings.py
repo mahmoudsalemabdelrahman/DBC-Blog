738c352f7ebc669f02eac4ea2165ad50ccfb132f
Mahmoud Salem <mahmoud19salem@gmail.com>
2023-10-04 00:29:40 +0300

    done verecl

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'blog_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'QGKtmvXH6SiXhl2Qvkmy',
+        'HOST':'containers-us-west-60.railway.app',
+        'PORT':'6256',
     }
 }
 
