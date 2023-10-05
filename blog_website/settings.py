4936283deff92db02df5129dd55c9c4202e23209
Mahmoud Salem <mahmoud19salem@gmail.com>
2023-10-05 16:38:40 +0300

    Depug False

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = '/media/'
