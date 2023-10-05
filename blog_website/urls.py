5e287ec7becfe86d21d41118dd6fbb8225db2a20
Mahmoud Salem <mahmoud19salem@gmail.com>
2023-10-05 16:17:29 +0300

    import ahitenoies

--- blog_website/urls.py
+++ blog_website/urls.py
@@ -17,13 +17,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('', include('user_profile.urls')),
+    path('media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
