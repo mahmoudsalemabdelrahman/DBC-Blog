d71141c56fd3f9574fc1fd8f1b7131079c329701
Mahmoud Salem <mahmoud19salem@gmail.com>
2023-10-06 12:18:40 +0300

    database

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =os.environ.get('SECRET_KEY', 'django-insecure-0ebap7n0ozdz48o9)*i9v-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=="True"
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost',"dbc-blog.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
