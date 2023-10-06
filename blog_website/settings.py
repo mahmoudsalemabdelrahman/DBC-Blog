be01a9bc0675d06cbef1a23bc30bb21a345c211d
Mahmoud Salem <mahmoud19salem@gmail.com>
2023-10-06 11:36:38 +0300

    secret key

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0ebap7n0ozdz48o9)*i9v-i&ss@tvsetv4^(!wuoiton@aaux6'
+SECRET_KEY =os.environ.get('SECRET_KEY', 'django-insecure-0ebap7n0ozdz48o9)*i9v-i&ss@tvsetv4^(!wuoiton@aaux6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=="True"
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'user_profile',
     'notification',
     'ckeditor',
+    'dj_database_url',
 ]
 
 MIDDLEWARE = [
